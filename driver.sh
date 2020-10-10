3e63ab285c6288da2185c9424a399551f571884a
batuhanaky <batuhanaky@gmail.com>
2020-10-10 03:02:44 +0300

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -2,4 +2,3 @@ cd /tmp/
 sudo wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
-chromedriver – version
