a4246ccdc6a189c9e6bb949e8f9d0630ca015001
batuhanaky <batuhanaky@gmail.com>
2020-10-10 03:01:14 +0300

    Create driver.sh

---
+++ driver.sh
@@ -0,0 +1,5 @@
+cd /tmp/
+sudo wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+chromedriver – version
